d475cd07d6532d83fa9efa7e9568b56f95c3c709
oo7-0310 <63403642+oo7-0310@users.noreply.github.com>
2021-11-20 23:24:00 +0900

    Update python_week5.py

--- python_week5.py
+++ python_week5.py
@@ -17,11 +17,14 @@ num=1
 while(num<=50):
     btn = driver.find_elements_by_xpath('//*[@id="grid"]/div[*]')  # *은 모두를 뜻함, elements -> list 형식으로 가져옴
     for i in btn:
-        if i.text == str(num):
+        if i.text == str(num):#태그 안의 text 값과 num을 비교하여 1부터 차례대로 클릭할 수 있도록
             i.click()
             print("클릭"+i.text)
             num+=1
-            time.sleep(5)
+            time.sleep(5)#이유는 모르겠는데 time.sleep 안해주면 브라우저가 금방 꺼짐(클릭 4번하고 꺼짐)
+            # time.sleep해도 꺼질 때도 있고 인자값에 따라 다름
+            # 50까지 클릭해도 다음 화면으로 안 넘어가고 꺼짐
+            # driver.implicitly_wait(10)이것도 안됨
             
 
     
